0a5b5cbd1ad2956b61221e630f9fdf80a2f0fb24
samigrace111 <66503648+samigrace111@users.noreply.github.com>
2025-04-14 17:34:08 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,3 +1,28 @@
+# https://github.com/samigrace111/lab10-XX-XX
+# Partner 1: Samantha Sobrino
+# Partner 2:
+
+import unittest
+from calculator import add, subtract, multiply, divide
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 4), 8)
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+        self.assertEqual(divide(5, 0), "Error: Cannot divide by zero")
+
+if __name__ == '__main__':
+    unittest.main()
+
 import unittest
 from calculator import *
 
