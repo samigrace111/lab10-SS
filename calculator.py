2f1dbf33682e0010d2a7c198020a08f9cd11e535
samigrace111 <66503648+samigrace111@users.noreply.github.com>
2025-04-14 17:32:14 -0400

    Update calculator.py
    put in arithmetic functions

--- calculator.py
+++ calculator.py
@@ -1,3 +1,23 @@
+# https://github.com/samigrace111/lab10-SS
+# Partner 1: Samantha Sobrino
+# Partner 2:
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero"
+    return a / b
+
+
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -9,3 +29,4 @@ def add(a, b):
     pass
 
 
+Added header info to calculator.py
